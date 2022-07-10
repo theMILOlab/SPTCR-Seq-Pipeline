44fb882ef661b2cd2fbe535ffc1a675f430d242c
jkbenotmane <jkbenotmane@googlemail.com>
2022-07-10 18:46:58 +0200

    Declutter

--- SPTCR-seq Pipeline Scripts/UMI_Correction.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash 
-
-umi_corr="/media/jkbuntu/SAMSUNG2TB/Dropbox/KBJasim/Projects/Capture_Sequencing/Github SPTCR-seq Pipeline/SPTCR-Seq-Pipeline/SCRIPTS/umi_correction_from_summary.py"
-
-"${umi_corr}" \
-    
